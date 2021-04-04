497d6422d1256ad4c1b79ad37955a3563ad99460
YoungYuan1971 <young_yuan@hotmail.com>
2021-04-04 16:58:04 +0800

    Decorator

---
+++ Decorator.py
@@ -0,0 +1,22 @@
+def pwd(order):
+    def order_new():
+        pwd = input('Please input the password:')
+        if pwd == '123456':
+            order()
+        else:
+            print('Wrong password!')
+    return order_new
+
+
+@pwd
+def buy():
+    print('Successful buy!')
+
+
+@pwd
+def sell():
+    print('Successful sell!')
+
+
+if __name__ == '__main__':
+    sell()
