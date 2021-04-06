1bf442170fae40189c78bdb21ceab7d346f4a148
YoungYuan1971 <young_yuan@hotmail.com>
2021-04-06 13:34:49 +0800

    Clock

---
+++ Clock.py
@@ -0,0 +1,30 @@
+import time
+import tkinter as tk
+
+
+class Clock(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Clock')
+        self.resizable(0, 0)
+        self.time_text = ""
+        self.lbl = tk.Label(
+            self,
+            text=self.time_text,
+            font=("ds-digital", 80),
+            background="black",
+            foreground="cyan"
+        )
+
+        self.lbl.pack()
+        self.update_time()
+
+    def update_time(self):
+        self.time_text = time.strftime("%Y-%m-%d\n%H:%M:%S %a")
+        self.lbl.config(text=self.time_text)
+        self.after(1000, self.update_time)
+
+
+if __name__ == '__main__':
+    clock = Clock()
+    clock.mainloop()
