0c3502dca066230fc769502f54f0f3db43e38f95
YoungYuan1971 <young_yuan@hotmail.com>
2021-04-13 10:15:12 +0800

    Python Accelerator

---
+++ Python_accelerator.py
@@ -0,0 +1,19 @@
+from numba import jit
+from time import time
+
+# 不加速4.80, 加速0.05
+
+
+@jit
+def func(x):
+    n = 0
+    for i in range(x):
+        n += i
+    return n
+
+
+if __name__ == "__main__":
+    start_time = time()
+    result = func(100000000)
+    print(result)
+    print(time()-start_time)
