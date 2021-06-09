4a2d849b9158d0b7ce94f1807d924d874ab40892
YoungYuan1971 <young_yuan@hotmail.com>
2021-06-09 22:39:48 +0800

    Request header processing

--- 网页请求头批量处理.py
+++ 网页请求头批量处理.py
@@ -5,13 +5,20 @@
 import re
 
 old_headers = '''
-
+Accept: image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
+Cache-Control: no-cache
+Connection: keep-alive
 
 
 '''
 
-pattern = '^(.*?):[\s]*(.*?)$'
+pattern = r'^(.*?):[\s]*(.*?)$'
+compile = r"'\1': '\2',"
 headers = ""
+
 for line in old_headers.splitlines():
-    headers += (re.sub(pattern, '\'\\1\': \'\\2\',', line)) + '\n'
+    headers += (re.sub(pattern, compile, line)) + '\n'
+
 print(headers[:-2])
