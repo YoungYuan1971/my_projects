cb727ddf06af5c0b3acdbea4ce5789f0de2bce14
YoungYuan1971 <young_yuan@hotmail.com>
2021-06-11 15:06:32 +0800

    Stock Get

--- Stock_Get.py
+++ Stock_Get.py
@@ -45,13 +45,14 @@ async def web_get(page):
         async with session.get('http://67.push2.eastmoney.com/api/qt/clist/get', headers=headers,
                                params=params) as response:
 
-            response = await response.json()
-            return response
+            content = await response.json()
+            return content
 
 
 async def data_get(page):
     async with aiofiles.open('Stock.csv', 'a', encoding='utf-8', newline='') as fin:
         data_writer = csv.writer(fin)
+        
         res_dic = await web_get(page)
         datas = res_dic['data']['diff']
         for data in datas:
@@ -77,12 +78,13 @@ async def main(pages):
 
 
 if __name__ == '__main__':
-    # request the one page, get the total number of pages
     base_url = 'http://39.push2.eastmoney.com/api/qt/clist/get?' \
         'pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281' \
         '&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23' \
         '&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,' \
         'f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152'
+
+    # request the one page, get the total number of pages
     html = requests.get(base_url, headers=headers)
     total = html.json()['data']['total']
     pages = math.ceil(int(total) / 20)
