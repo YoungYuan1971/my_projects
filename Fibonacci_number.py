4da3cc47167f47d5210ba8dc6b715995e04413a5
YoungYuan1971 <young_yuan@hotmail.com>
2021-06-23 10:54:37 +0800

    Fibonacci number

--- Fibonacci_number.py
+++ Fibonacci_number.py
@@ -1,14 +1,11 @@
 def fib(n):
     a, b = 0, 1
-    fib_list = []
 
     for _ in range(n):
         a, b = b, a+b
-        fib_list.append(a)
-
-    print(fib_list)
+        yield a
 
 
 if __name__ == '__main__':
     number = int(input('Please enter the number to generate: '))
-    fib(number)
+    print(list(fib(number)))
