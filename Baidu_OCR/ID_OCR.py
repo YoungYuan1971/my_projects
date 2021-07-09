4c32f844fb690a2e7ec7cd863b45ca063b7bc0d0
YoungYuan1971 <young_yuan@hotmail.com>
2021-07-09 14:27:37 +0800

    ID_OCR

--- Baidu_OCR/ID_OCR.py
+++ Baidu_OCR/ID_OCR.py
@@ -1,35 +1,80 @@
+import os
+import pandas as pd
 from aip import AipOcr
-from SecretData import BAIDU_Account
+from SecretData import BAIDU_OCR
+
+APP_ID = BAIDU_OCR['APP_ID']
+API_KEY = BAIDU_OCR['API_KEY']
+SECRET_KEY = BAIDU_OCR['SECRET_KEY']
+CLIENT = AipOcr(APP_ID, API_KEY, SECRET_KEY)  # 传递用户信息并实例化对象
 
 
-APP_ID = BAIDU_Account['APP_ID']
-API_KEY = BAIDU_Account['API_KEY']
-SECRET_KEY = BAIDU_Account['SECRET_KEY']
-CLIENT = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+def find_all_img():
+    base_path = '身份证/'
+    for root, _, fs in os.walk(base_path):
+        for f in fs:
+            if f.endswith(('.jpeg', '.jpg', '.png')):
+                yield root + f
 
-def ocr_run():
-    with open('/Users/youngyuan/PythonProjects/身份证识别_OCR/身份证/ID_001.jpeg', 'rb') as fp:
-        res = CLIENT.basicAccurate(fp.read())
-        for data in res['words_result']:
-            # print(data)
-            row = data['words']
-            if '姓名' in row:
-                name = row[2:]
-            elif '性别' in row:
-                gender = row[2:3]
-                nation = row[5:]
-            elif '出生' in row:
-                birth = row[2:]
-            elif '住址' in row:
-                addr = row[2:]
-            elif '公民身份号码' in row:
-                id = row[6:]
-            else:
-                addr += row
 
-        return name, gender, nation, birth, addr, id
+def img_ocr(img):
+    try:
+        with open(img, 'rb') as fp:
+            # res = client.basicAccurate(fp.read())  # 高精度，500次/天
+            res = CLIENT.basicGeneral(fp.read())  # 标准版，50000次/天
+            for data in res['words_result']:
+                # print(data)
+                row = data['words']
+                if '姓名' in row:
+                    name = row[2:]
+                elif '性别' in row:
+                    gender = row[2:3]
+                    nation = row[5:]
+                elif '出生' in row:
+                    birth = row[2:]
+                elif '住址' in row:
+                    addr = row[2:]
+                elif '公民身份号码' in row:
+                    ids = row[6:]
+                else:
+                    addr += row
+
+            data_info = {
+                '姓名': name,
+                '性别': gender,
+                '民族': nation,
+                '出生日期': birth,
+                '住址': addr,
+                '身份证号码': ids,
+                '文件名': img
+            }
+
+            return data_info
+
+    except:
+        fail_path = 'Fail/'
+        if not os.path.exists(fail_path):
+            os.mkdir(fail_path)
+        os.system(f'mv {img} {fail_path}/')  # 将识别失败的文件转移到统一目录下
+        print(f'{img}识别失败，已移到{fail_path}目录下！')
+
+        return False
+
+
+def main():
+    imgs = find_all_img()
+    datas = []
+    for img in imgs:
+        print(f'正在识别{img}......')
+        result = img_ocr(img)
+        if not result:
+            continue
+        datas.append(result)
+
+    df = pd.DataFrame(datas)
+    df.to_excel('./ID.xlsx', index=False)
+    print('识别完成！')
 
 
 if __name__ == '__main__':
-    result = list(ocr_run())
-    print(result)
+    main()
