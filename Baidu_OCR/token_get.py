2e2be3d20aaa4121361480e86e960caab52f57f1
YoungYuan1971 <young_yuan@hotmail.com>
2021-07-09 11:52:54 +0800

    Get the token

--- Baidu_OCR/token_get.py
+++ Baidu_OCR/token_get.py
@@ -5,8 +5,16 @@ from SecretData import BAIDU_Account
 AK = BAIDU_Account['API_KEY']
 SK = BAIDU_Account['SECRET_KEY']
 
-host = f'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={AK}&client_secret={SK}'
-response = requests.get(host)
-if response.status_code == 200:
-    result = response.json()
-    print(result['access_token'])
+
+def token_get():
+    host = f'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={AK}&client_secret={SK}'
+    response = requests.get(host)
+    if response.status_code == 200:
+        ressult = response.json()
+        access_token = ressult['access_token']
+
+        return access_token
+
+
+if __name__ == '__main__':
+    print(token_get())
