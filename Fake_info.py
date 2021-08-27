04c34218db1496e6c34403a8e41a36a3cd4bcccd
YoungYuan1971 <young_yuan@hotmail.com>
2021-08-27 18:09:37 +0800

    Updated

--- Fake_info.py
+++ Fake_info.py
@@ -13,13 +13,13 @@ def generate(n):
     datas = []
     for _ in tqdm(range(n)):
         data_info = {
-            '姓名': fake.name(),
-            '电话': fake.phone_number(),
-            '出生日期': fake.date(),
-            '公司': fake.company(),
-            '职位': fake.job(),
-            '地址': fake.address(),
-            '电子邮件': fake.email(),
+            'Name': fake.name(),
+            'Telephone': fake.phone_number(),
+            'Birth': fake.date(),
+            'Company': fake.company(),
+            'Position': fake.job(),
+            'Address': fake.address(),
+            'Email': fake.email(),
         }
         datas.append(data_info)
 
@@ -28,6 +28,6 @@ def generate(n):
 
 if __name__ == '__main__':
     total = 100000
-    fake = Faker(locale='zh_CN')
+    fake = Faker() # 设置中文：locale='zh_CN'
     df = pd.DataFrame(generate(total))
     df.to_excel('fake_info.xlsx', index=False)
