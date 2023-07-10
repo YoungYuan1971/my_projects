2435efa135efde2ec44740b2fe900ad4a918851d
YoungYuan <young_yuan@hotmail.com>
2023-07-10 08:58:26 +0800

    updated

--- iptv_get.py
+++ iptv_get.py
@@ -4,5 +4,5 @@ url = "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u
 
 res = requests.get(url)
 
-with open("/mnt/sda2/iptv/iptv.m3u", mode='w', encoding='utf-8') as f:
+with open("iptv.m3u", mode='w', encoding='utf-8') as f:
     f.write(res.text)
