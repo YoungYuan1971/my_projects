aa7e493f7c69feb2e5e06c5e4ba1132fe289a4c7
YoungYuan <young_yuan@hotmail.com>
2023-08-22 08:53:00 +0800

    updated

--- iptv_get.py
+++ iptv_get.py
@@ -17,15 +17,18 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.1 Safari/605.1.15',
 }
 
-while True:
+try:
     res = requests.get(url, headers=headers)
-    if res.status_code == 200:
-        break
-    else:
-        time.sleep(1)
 
-with open("iptv.m3u", mode='w', encoding='utf-8') as f:
-    f.write(res.text)
+    with open("iptv.m3u", mode='w', encoding='utf-8') as f:
+        f.write(res.text)
+        
+    update_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(f"于{update_time}...下载成功！")
 
-update_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-print(f"于{update_time}...下载成功！")
+except Exception as e:
+    print(f"下载失败！{e}")
+    time.sleep(1)
+    pass
+
+
