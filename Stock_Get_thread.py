3d6c7d6c223cddb20bcc47b22ab1c4c93cdadd17
YoungYuan1971 <young_yuan@hotmail.com>
2023-11-23 20:17:24 +0800

    updated

--- Stock_Get_thread.py
+++ Stock_Get_thread.py
@@ -12,29 +12,31 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def html_get(page):
-    url = 'https://63.push2.eastmoney.com/api/qt/clist/get'
-    headers = {
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-TW,zh;q=0.9,zh-CN;q=0.8',
-        'Connection': 'keep-alive',
-        'Cookie': 'qgqp_b_id=62c04a4e98d54385d4f24409507674b5; intellpositionL=1237.58px; cowminicookie=true; '
+headers = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-TW,zh;q=0.9,zh-CN;q=0.8',
+    'Connection': 'keep-alive',
+    'Cookie': 'qgqp_b_id=62c04a4e98d54385d4f24409507674b5; intellpositionL=1237.58px; cowminicookie=true; '
                   'cowCookie=true; st_si=73868227870871; st_asi=delete; intellpositionT=455px; st_pvi=76856893471525; '
                   'st_sp=2021-11-10%2008%3A32%3A30; st_inirUrl=http%3A%2F%2Fdata.eastmoney.com%2Fcenter%2F; st_sn=12; '
                   'st_psi=20211124105240665-113200301321-5546042617',
-        'DNT': '1',
-        'Host': '63.push2.eastmoney.com',
-        'Referer': 'https://quote.eastmoney.com/',
-        'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'Sec-Fetch-Dest': 'script',
-        'Sec-Fetch-Mode': 'no-cors',
-        'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'DNT': '1',
+    'Host': '63.push2.eastmoney.com',
+    'Referer': 'https://quote.eastmoney.com/',
+    'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'Sec-Fetch-Dest': 'script',
+    'Sec-Fetch-Mode': 'no-cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/95.0.4638.69 Safari/537.36',
-    }
+}
+
+
+def html_get(page):
+    url = 'https://63.push2.eastmoney.com/api/qt/clist/get'
     params = {
         # 'cb': 'jQuery112402374539779714402_1637721974443',
         'pn': str(page),
@@ -47,9 +49,10 @@ def html_get(page):
         'fid': 'f3',
         'fs': 'm:0 t:6,m:0 t:80,m:1 t:2,m:1 t:23,m:0 t:81 s:2048',
         'fields': 'f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,'
-                  'f136,f115,f152',
+        'f136,f115,f152',
         # '_': '1637721974505',
     }
+
     response = requests.get(url, headers=headers, params=params)
     return response.json()
 
@@ -87,7 +90,7 @@ def main():
          for task in tqdm(as_completed(tasks), total=len(tasks))]
 
     df = pd.DataFrame(results)
-    df.to_csv('stock.csv', index=False)
+    df.to_csv('stock_info.csv', index=False)
 
     print("数据信息已下载完成！")
 
