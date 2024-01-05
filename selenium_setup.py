4f6d2af1b795ed4de824de503d0a4dc27726e687
YoungYuan1971 <young_yuan@hotmail.com>
2024-01-05 15:45:40 +0800

    updated

--- selenium_setup.py
+++ selenium_setup.py
@@ -17,6 +17,8 @@ get: () => undefined
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 from time import sleep
 
 
