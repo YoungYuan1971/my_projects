217571f7c7412cca7cf0e707a2a170f16b777a60
YoungYuan1971 <young_yuan@hotmail.com>
2024-05-04 11:40:27 +0800

    updated

--- Test_async.py
+++ Test_async.py
@@ -3,30 +3,30 @@ import aiohttp
 import aiofiles
 
 urls = [
-    "http://kr.shanghai-jiuxin.com/file/2020/1031/774218be86d832f359637ab120eba52d.jpg",
-    "http://kr.shanghai-jiuxin.com/file/2020/1031/191468637cab2f0206f7d1d9b175ac81.jpg",
-    "http://kr.shanghai-jiuxin.com/file/2020/1031/563337d07af599a9ea64e620729f367e.jpg",
-    "http://kr.shanghai-jiuxin.com/file/2020/1031/a2c58d6d726fb7ef29390becac5d8643.jpg",
-    "http://kr.shanghai-jiuxin.com/file/2020/1031/d7de3f9faf1e0ecdea27b73139fc8d3a.jpg",
-    "http://kr.shanghai-jiuxin.com/file/2021/0326/0311c15cb53c16319db5f0a7ceb68642.jpg",
+    "https://images.pexels.com/photos/22626143/pexels-photo-22626143.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/21751135/pexels-photo-21751135.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/20604045/pexels-photo-20604045.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/21529784/pexels-photo-21529784.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/20922644/pexels-photo-20922644.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/20545539/pexels-photo-20545539.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
 ]
 
 
-async def download(url):
+async def download(url, session):
     name = url.split('/')[-1]
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as reponse:
-            async with aiofiles.open(name, mode='wb') as f:
-                await f.write(await reponse.content.read())
-                print(name, 'Download complete!')
+    async with session.get(url) as reponse:
+        async with aiofiles.open(f'001_{name}.png', mode='wb') as f:
+            await f.write(await reponse.read())
+            print(name, 'Download complete!')
 
 
 async def main():
-    tasks = []
-    for url in urls:
-        tasks.append(asyncio.create_task(download(url)))
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in urls:
+            tasks.append(asyncio.create_task(download(url, session)))
 
-    await asyncio.wait(tasks)
+        await asyncio.wait(tasks)
 
 
 if __name__ == "__main__":
