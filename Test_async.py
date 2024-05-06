90198030b0c8d9bc2537fdc6eeffb2cfa09c6cd8
YoungYuan1971 <young_yuan@hotmail.com>
2024-05-06 09:25:44 +0800

    updated

--- Test_async.py
+++ Test_async.py
@@ -14,24 +14,33 @@ urls = [
 
 # 异步协程获取数据：response.read() 获取二进制字节流；response.text() 获取字符串；response.json() 获取JSON格式数据
 async def download(url, num, session):
+    # 创建异步请求上下文
     async with session.get(url) as reponse:
+        # 创建异步文件读写上下文
         async with aiofiles.open(f'00{num+1}.png', mode='wb') as f:
+            # 写入文件
             await f.write(await reponse.read())
+            # 返回执行状态
             return f"{url}...Download completed!"
 
 
 async def main():
+    # 创建异步会话上下文
     async with aiohttp.ClientSession() as session:
+        # 创建任务列表
         tasks = []
         for url_index, url in enumerate(urls):
             tasks.append(asyncio.create_task(
                 download(url, url_index, session)))
 
+        # 获取返回数据
         for t in asyncio.as_completed(tasks):
             print(await t)
 
+        # 等待所有任务完成
         await asyncio.wait(tasks)
 
 
 if __name__ == "__main__":
+    # 运行异步主函数
     asyncio.run(main())
